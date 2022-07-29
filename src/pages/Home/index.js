e53ffc2bd1b7907910f5046158e7d658a4421adf
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-07-29 05:35:39 -0300

    f

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,11 +7,13 @@ function Home(){
 const movies = [
     {
         title:'Spider man',
-        image_url: 'https://th.bing.com/th/id/R.9098c09932212ce01373ca545dba4bb1?rik=8P3FhprB3LGu9Q&pid=ImgRaw&r=0',
+        id:'1',
+        image_url: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/7D430eqZj8y3oVkLFfsWXGRcpEG.jpg',
 
     },
     {
         title:'Batmam',
+        id:'2',
         image_url: 'https://th.bing.com/th/id/R.9098c09932212ce01373ca545dba4bb1?rik=8P3FhprB3LGu9Q&pid=ImgRaw&r=0',
 
     },
@@ -39,26 +41,34 @@ const movies = [
 
 return(
     <Container><h1>Movies</h1>
-        <MovieList>
+     <MovieList>
 
         {movies.map(movie =>{
             return(
                 <Movie>
-
+                   
+                 <div id="1">
                  <a href="https://draft.blogger.com/blog/posts/1818788552960104722">
                     <img src={movie.image_url} alt={movie.title}/></a>
-                 <span>{movie.title}</span>
+                 <span>{movie.title}</span></div>
+                 <div id="2">
+                 <a href="https://www.npmjs.com/package/react-router-dom/v/5.2.0">
+                    <img src={movie.image_url} alt={movie.title}/></a>
+                 <span>{movie.title}</span></div>
 
 
 
-                </Movie>
-            )
-        })}
+                </Movie>)})
+            
+
+
+
+        }
 
     
 
 
-    </MovieList>
+     </MovieList>
 
     </Container>
 )
